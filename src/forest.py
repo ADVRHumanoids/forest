c0029a86dc2dc3c8914e5967ca964e5bb6ffd788
Arturo Laurenzi <arturo.laurenzi@iit.it>
2021-06-20 22:22:47 +0200

    entry point

--- src/forest.py
+++ src/forest.py
@@ -5,35 +5,50 @@ import os
 
 from common.install import install_package
 
-# parse cmd line args
-parser = argparse.ArgumentParser(description='forest automatizes cloning and building of software packages')
-parser.add_argument('recipe', help='name of recipe with fetch and build information')
-parser.add_argument('--verbose', '-v', required=False, action='store_true', help='print additional information')
-args = parser.parse_args()
+# just a try-except wrapper to catch ctrl+c
+def main():
+    try:
+        return do_main()
+    except KeyboardInterrupt:
+        print('\nfailed (interrupted by user)')
+        return False
 
-# verbose mode will show output of any called process
-if args.verbose:
-    from common import proc_utils
-    proc_utils.call_process_verbose = True
+# actual main
+def do_main():
 
-# define directories for source, build, install
-rootdir = os.getcwd()
-buildroot = os.path.join(rootdir, 'build')
-installdir = os.path.join(rootdir, 'install')
-srcroot = os.path.join(rootdir, 'src')
-buildtype = 'Release'
+    # parse cmd line args
+    parser = argparse.ArgumentParser(description='forest automatizes cloning and building of software packages')
+    parser.add_argument('recipe', help='name of recipe with fetch and build information')
+    parser.add_argument('--verbose', '-v', required=False, action='store_true', help='print additional information')
+    args = parser.parse_args()
 
-# create directories
-for dir in (buildroot, installdir, srcroot):
-    if not os.path.exists(dir):
-        os.mkdir(dir)
+    # verbose mode will show output of any called process
+    if args.verbose:
+        from common import proc_utils
+        proc_utils.call_process_verbose = True
 
-# perform required installation
-success = install_package(pkg=args.recipe, 
-    srcroot=srcroot, 
-    buildroot=buildroot,
-    installdir=installdir,
-    buildtype=buildtype)
+    # define directories for source, build, install
+    rootdir = os.getcwd()
+    buildroot = os.path.join(rootdir, 'build')
+    installdir = os.path.join(rootdir, 'install')
+    srcroot = os.path.join(rootdir, 'src')
+    buildtype = 'Release'
 
-# return value
-exit(0 if success else 1)
+    # create directories
+    for dir in (buildroot, installdir, srcroot):
+        if not os.path.exists(dir):
+            os.mkdir(dir)
+
+    # perform required installation
+    success = install_package(pkg=args.recipe, 
+        srcroot=srcroot, 
+        buildroot=buildroot,
+        installdir=installdir,
+        buildtype=buildtype)
+
+    return success
+
+
+if __name__ == '__main__':
+    # return value
+    exit(0 if main() else 1)
