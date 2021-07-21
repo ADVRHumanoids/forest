754f43fa3a97daba8b240915da1400931b0d069e
Arturo Laurenzi <arturo.laurenzi@iit.it>
2021-07-21 16:07:46 +0200

    [bugfix] build dep

--- src/forest/common/install.py
+++ src/forest/common/install.py
@@ -24,6 +24,7 @@ def build_package(pkg: package.Package,
                       buildtype=buildtype, jobs=jobs, reconfigure=reconfigure)
 
 
+
 # function to install one package with dependencies
 def install_package(pkg: str,
                     srcroot: str,
@@ -56,14 +57,25 @@ def install_package(pkg: str,
         dep_builddir = os.path.join(buildroot, dep)
 
         if not dep_found:
+            # dependency not found -> install it
             print(f'[{pkg.name}] depends on {dep} -> not found, installing..')
             ok = install_package(dep, srcroot, buildroot, installdir, buildtype, jobs, reconfigure)   # reconfigure needed if there's build but not install
             if not ok:
                 print(f'[{pkg.name}] failed to install dependency {dep}')
                 return False
         elif os.path.exists(dep_builddir):
+            # dependency found and built by forest -> trigger build
             print(f'[{pkg.name}] depends on {dep} -> build found, building..')   
-            ok = build_package(pkgname=dep, 
+
+            # retrieve package info from recipe
+            try:
+                debpkg = package.Package.from_name(name=dep)
+            except FileNotFoundError:
+                print(f'[{pkg}] recipe file not found (searched in {package.Package.get_recipe_path()})')
+                return False
+            
+            # build
+            ok = build_package(pkg=debpkg, 
                                srcroot=srcroot, 
                                buildroot=buildroot, 
                                installdir=installdir, 
@@ -74,6 +86,7 @@ def install_package(pkg: str,
                 print(f'[{pkg.name}] failed to build dependency {dep}')
                 return False 
         else:
+            # dependency found and not built by forest -> nothing to do
             print(f'[{pkg.name}] depends on {dep} -> found')
     
     # use the fetcher!
