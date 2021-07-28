bd66407401da3df2b5ce0835ebac7616a3687075
Arturo Laurenzi <arturo.laurenzi@iit.it>
2021-07-28 13:47:25 +0200

    proc_utils.get_output

--- src/forest/common/proc_utils.py
+++ src/forest/common/proc_utils.py
@@ -23,3 +23,22 @@ def call_process(args, cwd='.', input=None, verbose=False, print_on_error=True):
         return False
 
     return True
+
+
+def get_output(args, cwd='.', input=None, verbose=False, print_on_error=True):
+
+    if verbose or call_process_verbose:
+        print('calling "{}"'.format(' '.join(args)))
+
+    try:
+        # check_output will not print
+        out = subprocess.check_output(args=args, cwd=cwd, input=input)
+        ret = out.decode().strip()
+        if verbose or call_process_verbose:
+            print('calling "{}" returned "{}"'.format(' '.join(args), ret))
+        return ret
+    except subprocess.CalledProcessError as e:
+        # on error, print output
+        if print_on_error:
+            print(e.output.decode(), file=sys.stderr)
+        return None
