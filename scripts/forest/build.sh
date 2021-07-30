51d4f006bf543ba10209405a9437e727bc5fec80
Marco Ruzzon <marco.ruzzon@iit.it>
2021-07-30 15:28:05 +0200

    (docker): update script to be run inside docker container

--- scripts/forest/build.sh
+++ scripts/forest/build.sh
@@ -1,3 +1,5 @@
+set -e   # return 1 if a command fails
+
 # Add all ssh keys in .ssh to ssh client agent
 eval "$(ssh-agent -s)"
 for f in $HOME/.ssh/*
@@ -25,14 +27,28 @@ source setup.sh
 cd ..
 # ----------------------- TO BE REMOVED
 
-cd forest || return 1
+cd forest
 # pip3 install --user -e .
 # export PATH=$PATH:/home/user/.local/bin
 pip3 install -e .   # system installation (root)
 cd ..
 mkdir xbot2_forest
-cd xbot2_forest || return 1
+cd xbot2_forest
 forest --init
 source install/setup.bash
-forest --add-recipes git@github.com:MarcoRuzzon/forest-recipes.git main
-forest xbot2_examples -j8
+forest --add-recipes git@github.com:MarcoRuzzon/forest-recipes.git xeno
+forest xbot2_examples -j8 -v
+
+forest --add-recipes git@github.com:MarcoRuzzon/forest-recipes.git robots
+forest ModularBot_conda_gripper -j8 -v
+
+set_xbot2_config install/share/ModularBot_conda_gripper/config/ModularBot.yaml
+roscore &
+roscore_pid="$!"
+sleep 5
+xbot2-core --hw dummy --verbose &
+xbot2_core_pid="$!"
+
+sleep 10
+kill $xbot2_core_pid
+kill $roscore_pid
