cdfbb1aeeccf5ad1501e05391b6ad4b2bbf46b2e
Marco Ruzzon <marco.ruzzon@iit.it>
2021-10-15 14:47:09 +0200

    fix issue19(setup.bash): remove lib/python2.7/site-packages from PYTHONPATH

--- src/forest/common/setup.bash
+++ src/forest/common/setup.bash
@@ -4,5 +4,5 @@ export CMAKE_PREFIX_PATH=£PREFIX£:$CMAKE_PREFIX_PATH
 export PATH=£PREFIX£/bin:$PATH
 export GAZEBO_PLUGIN_PATH=£PREFIX£/lib:$GAZEBO_PLUGIN_PATH
 export PYTHONPATH=£PREFIX£/lib/python2.7/dist-packages:£PREFIX£/lib/python3/dist-packages:$PYTHONPATH
-export PYTHONPATH=£PREFIX£/lib/python2.7/site-packages:£PREFIX£/lib/python3/site-packages:$PYTHONPATH
+export PYTHONPATH=£PREFIX£/lib/python3/site-packages:$PYTHONPATH
 export ROS_PACKAGE_PATH=£PREFIX£/share:£PREFIX£/lib:$ROS_PACKAGE_PATH
