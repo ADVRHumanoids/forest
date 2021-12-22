0a550070e6c60d6a2a69588bf5e8688323f3398d
Marco Ruzzon <marco.ruzzon@iit.it>
2021-12-22 12:51:42 +0100

    issue29: do not add src folder to ROS_PACKAGE_PATH when sourcing forest ws

--- src/forest/common/setup.bash
+++ src/forest/common/setup.bash
@@ -5,5 +5,5 @@ export PATH=£PREFIX£/bin:$PATH
 export GAZEBO_PLUGIN_PATH=£PREFIX£/lib:$GAZEBO_PLUGIN_PATH
 export PYTHONPATH=£PREFIX£/lib/python2.7/dist-packages:£PREFIX£/lib/python3/dist-packages:$PYTHONPATH
 export PYTHONPATH=£PREFIX£/lib/python3/site-packages:$PYTHONPATH
-export ROS_PACKAGE_PATH=£SRCDIR£/:£PREFIX£/share:£PREFIX£/lib:$ROS_PACKAGE_PATH
+export ROS_PACKAGE_PATH=£PREFIX£/share:£PREFIX£/lib:$ROS_PACKAGE_PATH
 export PKG_CONFIG_PATH=£PREFIX£/lib/pkgconfig:$PKG_CONFIG_PATH
