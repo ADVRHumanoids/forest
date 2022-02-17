2906c33c0fd76e20086736a7915a2e1de5213af2
Arturo Laurenzi <arturo.laurenzi@iit.it>
2022-02-17 15:35:08 +0100

    more tests and bugfixes

--- src/forest/common/fetch_handler.py
+++ src/forest/common/fetch_handler.py
@@ -94,7 +94,7 @@ class CustomFetcher(FetchHandler):
         with TemporaryDirectory(prefix="foresttmp-") as tmpdir:
             for cmd in self.commands:
                 cmd_p = eh.process_string(cmd, {'srcdir': srcdir})
-                if not proc_utils.call_process(cmd_p, cwd=tmpdir, shell=True, print_on_error=True):
+                if not proc_utils.call_process([cmd_p], cwd=tmpdir, shell=True, print_on_error=True):
                     self.pprint(f'{cmd_p} failed')
                     shutil.rmtree(srcdir, ignore_errors=True)
                     return False 
