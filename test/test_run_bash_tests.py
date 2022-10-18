bb04f54bc425537523413862905ed83d15978023
Arturo Laurenzi <arturo.laurenzi@iit.it>
2022-10-18 11:47:16 +0200

    unittest was giving weird issues -> barebone main!

--- test/test_run_bash_tests.py
+++ test/test_run_bash_tests.py
@@ -1,23 +1,27 @@
-from re import sub
-import unittest
 import os
+import sys
 import glob 
 import subprocess
 
-class TestForestBashWrapper(unittest.TestCase):
-
-    pass
-
-
 if __name__ == '__main__':
+
+    # set https as default 
+    os.putenv('HHCM_FOREST_CLONE_DEFAULT_PROTO', 'https')
+
     curr_dir = os.path.dirname(os.path.abspath(__file__))
     bash_test_files = glob.glob(curr_dir + '/test_*.bash')
     bash_test_files.sort()
-    print(f'found bash test files in {curr_dir}: {bash_test_files}')
+    sep = '\n  - '
+    print(f'found bash test files in {curr_dir}: {sep.join(bash_test_files)}')
+    
+    failed = False
     for f in bash_test_files:
-        def do_test(self):
-            print(f'\n\n\n>>>>>>> running test {f}')
+        print(f'\n\n\n>>>>>>> running test {f}')
+        try:
             subprocess.run(f, check=True)
-        setattr(TestForestBashWrapper, os.path.basename(f)[:-5], do_test)
+        except subprocess.CalledProcessError as e:
+            failed = True 
+            print(e)
 
-    unittest.main()
+    if failed:
+        sys.exit(1)
