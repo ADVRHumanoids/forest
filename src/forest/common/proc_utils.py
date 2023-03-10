3048809b512060c93a333c3da385fd2b8d915a60
Marco Ruzzon <marco.ruzzon@iit.it>
2023-03-10 16:01:20 +0100

    wip

--- src/forest/common/proc_utils.py
+++ src/forest/common/proc_utils.py
@@ -2,6 +2,7 @@ import subprocess
 import sys
 import getpass
 import re
+import typing
 
 from forest.common import print_utils
 
@@ -40,8 +41,7 @@ def call_process(args, cwd='.', input=None, verbose=False, print_on_error=True, 
                         cwd=cwd, 
                         input=input, 
                         shell=shell, 
-                        executable=executable,
-                        check=True)
+                        executable=executable)
 
         print_utils.log_file.write(pr.stdout.decode())
 
@@ -79,3 +79,59 @@ def get_output(args, cwd='.', input=None, verbose=False, print_on_error=True, sh
             print('stderr: ' + e.stderr.decode(), file=sys.stderr)
         return None
 
+
+def call_process_stdout_readline_cb(args: typing.List[str] = None, 
+                                    callable: typing.Callable[[str], None]=None, 
+                                    cwd='.', 
+                                    shell: bool=False,
+                                    timeout=None,
+                                    verbose=False) -> bool:    
+    # convert args to string
+    # args = list(map(str, args))    
+    if args is None:
+        args = []
+    
+    if verbose or call_process_verbose:
+        if shell:
+            print(f'calling shell with command "{args}"')
+        else:
+            print('calling "{}"'.format(' '.join(args)))
+    
+    if shell:
+        executable = '/bin/bash'
+        popen_args = ' ' + ' '.join(args) 
+    
+    else:
+        executable = None
+        popen_args = args
+
+    if call_process_verbose or verbose:
+        # run will print output to terminal
+        proc = subprocess.run(args=args, 
+                        cwd=cwd, 
+                        input=input, 
+                        shell=shell, 
+                        executable=executable)
+
+        print(f'returned {proc.returncode}')
+
+        return proc.returncode == 0 
+
+    proc = subprocess.Popen(args=popen_args,
+                            stdout=subprocess.PIPE,
+                            stderr=subprocess.STDOUT, 
+                            cwd=cwd, 
+                            shell=shell, 
+                            executable=executable,
+                            text=True)
+    
+    while True:
+        line = proc.stdout.readline()
+
+        if not line:
+            break
+        
+        if callable is not None:
+            callable(line)
+        
+    return proc.wait(timeout=timeout) == 0
